a5db0ebca9c835cd0dbcd553966d6c6f21918e19
Zero <zero.griffin@gmail.com>
2018-02-25 17:48:37 +0100

    Add files

---
+++ Source/Controller/Home/ControllerHome.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+internal final class ControllerHome:Controller<ArchitectureHome> {
+}
---
+++ Source/View/Home/ViewHome.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+internal final class ViewHome:View<ArchitectureHome> {
+    internal override func factoryViews() {
+        super.factoryViews()
+        self.backgroundColor = UIColor.red
+    }
+}
