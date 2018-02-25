9f7f5976c66bdf7e45d7f4ace35e69221481f28a
Zero <zero.griffin@gmail.com>
2018-02-25 18:02:53 +0100

    Remove architecture home

--- Source/Architecture/ArchitectureHome.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-internal struct ArchitectureHome:Architecture {
-    internal typealias GenericModel = ModelHome
-    internal typealias GenericView = ViewHome
-    internal typealias GenericController = ControllerHome
-}
