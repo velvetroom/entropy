45a0861b8d8d3ff3ae5261f7b484b378f7527d6a
Zero <zero.griffin@gmail.com>
2018-02-25 18:08:48 +0100

    Remove view home

--- Source/View/Home/ViewHome.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-internal final class ViewHome:View<ArchitectureHome> {
-    internal override func factoryViews() {
-        super.factoryViews()
-        self.backgroundColor = UIColor.red
-    }
-}
