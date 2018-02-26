81cabf5650779ecdb6ba846524108097cef37f77
Zero <zero.griffin@gmail.com>
2018-02-26 12:56:53 +0100

    Update presenting protocol

--- Source/Model/Presentation/PresentationProtocol.swift
+++ Source/Model/Presentation/PresentationProtocol.swift
@@ -1,7 +1,12 @@
 import UIKit
 
 internal protocol PresentationProtocol {
+    associatedtype ConcreteController:ControllerProtocol
+    
     var controller:UIViewController? { get }
     var view:UIView? { get }
+    var model:Model<ConcreteController> { get }
     var presentationStrategy:((Presenter) -> (PresentationProtocol) -> ()) { get }
+    
+    init(model:Model<ConcreteController>)
 }
