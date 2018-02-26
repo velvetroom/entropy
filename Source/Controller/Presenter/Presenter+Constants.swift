6f4b35cce38c5294a8cb5fd9422e345424973e80
Zero <zero.griffin@gmail.com>
2018-02-26 13:56:26 +0100

    Create constants extension

---
+++ Source/Controller/Presenter/Presenter+Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+internal extension Presenter {
+    internal struct Constants {
+        internal static let queueIdentifier:String = "iturbide.entropy.presenter.queue"
+    }
+}
