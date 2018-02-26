72b06a932506443288c570ec7f3099d46df147a5
Zero <zero.griffin@gmail.com>
2018-02-26 11:10:29 +0100

    Create controller delegate

--- Source/Controller/Controller/Controller.swift
+++ Source/Controller/Controller/Controller.swift
@@ -1,4 +1,11 @@
 import UIKit
 
 internal class Controller:UIViewController {
+    internal init() {
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder){
+        return nil
+    }
 }
