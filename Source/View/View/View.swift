3711b6864847b34b2fdad4c6c0f475cfa7a38c2a
Zero <zero.griffin@gmail.com>
2018-02-26 13:49:11 +0100

    Add presentation layout to view

--- Source/View/View/View.swift
+++ Source/View/View/View.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 internal class View:UIView {
+    internal var presentationLayout:PresentationLayout
     
+    internal required init() {
+        self.presentationLayout = PresentationLayout()
+        super.init(frame:CGRect.zero)
+    }
+    
+    internal required init?(coder:NSCoder) {
+        return nil
+    }
 }
