3ac41dd21bfba285260962c44cf394a312b87c5a
Zero <zero.griffin@gmail.com>
2018-02-28 13:24:48 +0100

    Add simulation to controller delegate

--- Source/Model/Simulation/SimulationControllerDelegate.swift
+++ Source/Model/Simulation/SimulationControllerDelegate.swift
@@ -2,4 +2,5 @@ import Foundation
 
 internal final class SimulationControllerDelegate:ControllerDelegate {
     internal weak var viewModel:ViewModelProtocol?
+    internal weak var model:Simulation?
 }
