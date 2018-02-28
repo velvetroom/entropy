908fcac77cc4c8d53a351c722e7ea0be5e2aba9d
Zero <zero.griffin@gmail.com>
2018-02-28 15:53:31 +0100

    Create application

--- Source/Simulation/SimulationSignature.swift
+++ Source/Simulation/SimulationSignature.swift
@@ -3,5 +3,5 @@ import Foundation
 internal struct SimulationSignature:AbstractSignature {
     internal typealias Model = Simulation
     internal typealias Controller = SimulationController
-    internal typealias ViewModel = <#type#>
+    internal typealias ViewModel = SimulationViewModel
 }
