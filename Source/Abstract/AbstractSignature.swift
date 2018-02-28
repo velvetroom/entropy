174109216533b6e6a5dfe196f903f7fcb4505820
Zero <zero.griffin@gmail.com>
2018-02-28 16:44:23 +0100

    Add signature to model

--- Source/Abstract/AbstractSignature.swift
+++ Source/Abstract/AbstractSignature.swift
@@ -1,8 +1,6 @@
 import Foundation
 
 internal protocol AbstractSignature {
-    associatedtype Signature:AbstractSignature
-    associatedtype Model:AbstractModel
     associatedtype Controller:AbstractController
     associatedtype ViewModel:AbstractViewModel
 }
--- Source/Simulation/Simulation.swift
+++ Source/Simulation/Simulation.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-internal final class Simulation:AbstractModel {
+internal final class Simulation:AbstractModel<SimulationSignature> {
     
 }
