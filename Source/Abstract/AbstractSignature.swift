e031643b90dcde08c7a4bf7ed8fca9202164c9db
Zero <zero.griffin@gmail.com>
2018-02-28 15:37:56 +0100

    Create simulation signature

--- Source/Abstract/AbstractSignature.swift
+++ Source/Abstract/AbstractSignature.swift
@@ -3,6 +3,5 @@ import Foundation
 internal protocol AbstractSignature {
     associatedtype Model:AbstractModel
     associatedtype Controller:AbstractController
-    associatedtype View:AbstractView
     associatedtype ViewModel:AbstractViewModel
 }
---
+++ Source/Simulation/SimulationSignature.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+internal struct SimulationSignature:AbstractSignature {
+    
+}
