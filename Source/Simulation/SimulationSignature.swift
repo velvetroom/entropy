2839bf59857415be4549e975a49566200b77a8b6
Zero <zero.griffin@gmail.com>
2018-03-01 07:53:56 +0100

    Remove simulation signature

--- Source/Simulation/SimulationSignature.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-internal struct SimulationSignature:AbstractSignature {
-    internal typealias Model = Simulation
-    internal typealias Controller = SimulationController
-    internal typealias ViewModel = SimulationViewModel
-}
