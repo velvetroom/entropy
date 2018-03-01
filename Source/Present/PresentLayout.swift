ad11846a0647718fd63ddc3982de2b7d688188c2
Zero <zero.griffin@gmail.com>
2018-03-01 09:25:26 +0100

    Create present layout

---
+++ Source/Present/PresentLayout.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+internal struct PresentLayout {
+    internal weak var layoutTop:NSLayoutConstraint?
+    internal weak var layoutBottom:NSLayoutConstraint?
+    internal weak var layoutLeft:NSLayoutConstraint?
+    internal weak var layoutRight:NSLayoutConstraint?
+}
