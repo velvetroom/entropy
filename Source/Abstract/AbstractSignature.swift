8823b0654f324410e137d0b9f6d8966ea3460f35
Zero <zero.griffin@gmail.com>
2018-03-01 07:53:39 +0100

    Remove signature

--- Source/Abstract/AbstractSignature.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-internal protocol AbstractSignature {
-    associatedtype Model
-    associatedtype ViewModel
-    associatedtype ViewController
-    associatedtype Controller:AbstractController<ViewModel, ViewController>
-}
