44027c10d48670b6c2bf8b9ed0f660a633e20109
Zero <zero.griffin@gmail.com>
2018-03-01 09:13:32 +0100

    Add view controller

--- Source/Present/PresentCentred.swift
+++ Source/Present/PresentCentred.swift
@@ -2,6 +2,6 @@ import Foundation
 
 internal final class PresentCentred:PresentProtocol {
     internal class func present(model:AbstractModel, viewController:ApplicationViewController) {
-
+        self.addViewController(model:model, viewController:viewController)
     }
 }
