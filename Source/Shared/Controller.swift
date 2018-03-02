f155b31657b68157fc572ef17f73dc1bedc784ad
zero <zero.griffin@gmail.com>
2018-03-02 12:00:00 +0100

    Add delegate to controller

--- Source/Shared/Controller.swift
+++ Source/Shared/Controller.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 internal class Controller<SpecialisedViewModel:AbstractViewModel>:AbstractController {
+    internal weak var delegate:ControllerDelegate?
     internal weak var viewModel:SpecialisedViewModel?
 }
