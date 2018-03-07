32f2ccc2f1aa391bc60beda36b55c38752ec1e68
Zero <zero.griffin@gmail.com>
2018-03-07 11:49:46 +0100

    Add models to list in presenter

--- Source/Application/ApplicationPresenter.swift
+++ Source/Application/ApplicationPresenter.swift
@@ -2,20 +2,31 @@ import Foundation
 
 internal final class ApplicationPresenter:ApplicationPresenterProtocol {
     internal weak var controller:ApplicationController?
+    internal var models:[Model]
     internal let queue:DispatchQueue
 
     internal init() {
+        self.models = []
         self.queue = ApplicationPresenter.factoryPrivateQueue()
     }
     
     internal func present(model:Model, presentStrategy:PresentProtocol.Type) {
-        DispatchQueue.main.async { [weak self] in
-            guard
-                let controller:ApplicationController = self?.controller
-            else {
-                return
-            }
-            presentStrategy.present(model:model, controller:controller)
+        self.queue.async { [weak self] in
+            self?.retainModel(model:model)
+            self?.presentWith(strategy:presentStrategy, model:model)
         }
+    }
+    
+    private func retainModel(model:Model) {
+        self.models.append(model)
+    }
+    
+    private func presentWith(strategy:PresentProtocol.Type, model:Model) {
+        guard
+            let controller:ApplicationController = self.controller
+        else {
+            return
+        }
+        strategy.present(model:model, controller:controller)
     }
 }
