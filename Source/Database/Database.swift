4fbe3dd78d7921d3a0a38888cd06272c383a9a35
Zero <zero.griffin@gmail.com>
2018-03-08 11:19:53 +0100

    Create provider core data

--- Source/Database/Database.swift
+++ Source/Database/Database.swift
@@ -3,7 +3,7 @@ import Foundation
 internal final class Database {
     internal let provider:DatabaseProviderProtocol
     
-    internal init() {
-        
+    internal init(provider:DatabaseProviderProtocol = DatabaseProviderCoredata()) {
+        self.provider = provider
     }
 }
