e6f06b6e688424d0c9f66f03cb66580871a8702d
Zero <zero.griffin@gmail.com>
2018-03-08 13:21:07 +0100

    Add tests for profile loading

--- tests/Database/TestDatabaseProviderCoredata.swift
+++ tests/Database/TestDatabaseProviderCoredata.swift
@@ -3,10 +3,46 @@ import XCTest
 
 internal class TestDatabaseProviderCoredata:XCTestCase {
     private var provider:DatabaseProviderCoredata?
+    private var profileLoaded:Profile?
+    private struct Constants {
+        fileprivate static let expectationWait:TimeInterval = 1
+    }
     
     internal override func setUp() {
         super.setUp()
         let bundle:Bundle = Bundle(for:TestDatabaseProviderCoredata.self)
         self.provider = DatabaseProviderCoredata(bundle:bundle)
+    }
+    
+    internal func testCreateProfile() {
+        self.waitToCreateProfile()
+        waitForExpectations(timeout:Constants.expectationWait) { [weak self] (error:Error?) in
+            XCTAssertNotNil(self?.profileLoaded, "Error: profile not created")
+        }
+    }
+    
+    private func waitToCreateProfile() {
+        let expect:XCTestExpectation = expectation(description:"Wait for profile to create")
+        self.provider?.createProfile { [weak self] (profile:Profile) in
+            self?.profileLoaded = profile
+            expect.fulfill()
+        }
+    }
+    
+    internal func testLoadProfile() {
+        self.waitToLoadProfile()
+        waitForExpectations(timeout:Constants.expectationWait) { [weak self] (error:Error?) in
+            XCTAssertNotNil(self?.profileLoaded, "Error: profile not loaded")
+        }
+    }
+    
+    private func waitToLoadProfile() {
+        let expect:XCTestExpectation = expectation(description:"Wait for profile to load")
+        self.provider?.loadProfile(found: { [weak self] (profile:Profile) in
+            self?.profileLoaded = profile
+            expect.fulfill()
+        }, notFound: {
+            expect.fulfill()
+        })
     }
 }
