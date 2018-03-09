901330f77f6670ba0618272327b54e663aefe324
Zero <zero.griffin@gmail.com>
2018-03-09 12:15:51 +0100

    Add graph view

--- Source/Simulation/SimulationView.swift
+++ Source/Simulation/SimulationView.swift
@@ -1,7 +1,18 @@
 import UIKit
 
 class SimulationView:AbstractView {
+    private(set) weak var viewGraph:SimulationViewGraph?
+    
     override func factoryViews() {
         super.factoryViews()
+        
+        let viewGraph:SimulationViewGraph = SimulationViewGraph()
+        self.viewGraph = viewGraph
+        
+        self.addSubview(viewGraph)
+        
+        viewGraph.layoutTopToTop(view:self)
+        viewGraph.layoutHeight(constant:Constants.graphHeight)
+        viewGraph.layoutEqualsHorizontal(view:self)
     }
 }
--- Source/Simulation/SimulationViewGraph.swift
+++ Source/Simulation/SimulationViewGraph.swift
@@ -1,5 +1,14 @@
 import UIKit
 
-class SimulationViewGraph {
+class SimulationViewGraph:UIView {
+    init() {
+        super.init(frame:CGRect.zero)
+        self.clipsToBounds = true
+        self.backgroundColor = UIColor.clear
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
     
+    required init?(coder:NSCoder) {
+        return nil
+    }
 }
