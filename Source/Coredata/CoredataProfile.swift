3326d50aa360664a52f8fc3229ce0daac9f28ce5
Zero <zero.griffin@gmail.com>
2018-03-09 08:28:58 +0100

    Create coredata profile extension

---
+++ Source/Coredata/CoredataProfile.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension CoredataProfile {
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+    }
+}
--- Source/Coredata/CoredataProject.swift
+++ Source/Coredata/CoredataProject.swift
@@ -2,6 +2,7 @@ import Foundation
 
 extension CoredataProject {
     public override func awakeFromInsert() {
+        super.awakeFromInsert()
         self.created = Date()
         self.start = self.created
         self.name = String.localizedCoredata(key:"CoredataProject_name")
