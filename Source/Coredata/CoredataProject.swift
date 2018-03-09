7b6c952bfd403d455984992b876dc65840517efa
Zero <zero.griffin@gmail.com>
2018-03-09 10:25:05 +0100

    Generate identifier on insert

--- Source/Coredata/CoredataProject.swift
+++ Source/Coredata/CoredataProject.swift
@@ -3,6 +3,7 @@ import Foundation
 extension CoredataProject {
     public override func awakeFromInsert() {
         super.awakeFromInsert()
+        self.identifier = UUID().uuidString
         self.created = Date()
         self.start = self.created
         self.name = String.localizedCoredata(key:"CoredataProject_name")
