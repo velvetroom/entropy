cdb04d1e507ce7c3687b51e11f3a35c2cd945770
Zero <zero.griffin@gmail.com>
2018-03-11 18:32:51 +0100

    Add view scroll to simulation

--- Source/Simulation/SimulationView.swift
+++ Source/Simulation/SimulationView.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class SimulationView:AbstractView {
     private(set) weak var viewGraph:SimulationViewGraph?
+    private(set) weak var viewScroll:SimulationViewScroll?
     
     override func factoryViews() {
         super.factoryViews()
@@ -9,10 +10,16 @@ class SimulationView:AbstractView {
         let viewGraph:SimulationViewGraph = SimulationViewGraph()
         self.viewGraph = viewGraph
         
+        let viewScroll:SimulationViewScroll = SimulationViewScroll()
+        self.viewScroll = viewScroll
+        
         self.addSubview(viewGraph)
+        self.addSubview(viewScroll)
         
         viewGraph.layoutTopToTop(view:self)
         viewGraph.layoutHeight(constant:Constants.graphHeight)
         viewGraph.layoutEqualsHorizontal(view:self)
+        
+        viewScroll.layoutEquals(view:self)
     }
 }
