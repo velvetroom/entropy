3974a6691c50ef99a7672797a7ee078257c89328
Zero <zero.griffin@gmail.com>
2018-03-11 12:17:24 +0100

    Update colours

--- Source/Extension/ExtensionUIColor.swift
+++ Source/Extension/ExtensionUIColor.swift
@@ -3,6 +3,6 @@ import UIKit
 extension UIColor {
     static let sharedBackgroundColour:UIColor = UIColor.white
     static let sharedGrayColour:UIColor = UIColor(red:0.92, green:0.94, blue:0.96, alpha:1)
-    static let sharedChaosColour:UIColor = UIColor(red:1, green:0.18, blue:0, alpha:1)
-    static let sharedProductivityColour:UIColor = UIColor(red:0.3, green:0.5, blue:0.7, alpha:1)
+    static let sharedChaosColour:UIColor = UIColor(red:1, green:0.22, blue:0, alpha:1)
+    static let sharedProductivityColour:UIColor = UIColor(red:0.3, green:0.65, blue:0.85, alpha:1)
 }
