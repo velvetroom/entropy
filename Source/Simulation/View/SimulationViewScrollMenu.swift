0e40f5025470fa6429c3185523cb4fad13957ab5
Zero <zero.griffin@gmail.com>
2018-03-11 22:04:50 +0100

    Layout view menu on cell

--- Source/Simulation/View/SimulationViewScrollMenu.swift
+++ Source/Simulation/View/SimulationViewScrollMenu.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class SimulationViewScrollMenu:UICollectionViewCell {
+    private(set) weak var viewMenu:SimulationViewMenu?
+    
     override init(frame:CGRect) {
         super.init(frame:frame)
         self.clipsToBounds = true
@@ -16,8 +18,12 @@ class SimulationViewScrollMenu:UICollectionViewCell {
         viewBorderBottom.isUserInteractionEnabled = false
         viewBorderBottom.backgroundColor = UIColor.sharedGrayColour
         
+        let viewMenu:SimulationViewMenu = SimulationViewMenu()
+        self.viewMenu = viewMenu
+        
         self.addSubview(viewBorderTop)
         self.addSubview(viewBorderBottom)
+        self.addSubview(viewMenu)
         
         viewBorderTop.layoutTopToTop(view:self)
         viewBorderTop.layoutHeight(constant:1)
@@ -26,6 +32,9 @@ class SimulationViewScrollMenu:UICollectionViewCell {
         viewBorderBottom.layoutBottomToBottom(view:self)
         viewBorderBottom.layoutHeight(constant:1)
         viewBorderBottom.layoutEqualsHorizontal(view:self)
+        
+        viewMenu.layoutEqualsVertical(view:self, margin:1)
+        viewMenu.layoutEqualsHorizontal(view:self)
     }
     
     required init?(coder:NSCoder) {
