4e8387f40549bd562769184adce323e5cbffcd03
Zero <zero.griffin@gmail.com>
2018-03-11 19:48:46 +0100

    Add top border to menu view

--- Source/Simulation/SimulationViewScrollMenu.swift
+++ Source/Simulation/SimulationViewScrollMenu.swift
@@ -4,7 +4,18 @@ class SimulationViewScrollMenu:UICollectionViewCell {
     override init(frame:CGRect) {
         super.init(frame:frame)
         self.clipsToBounds = true
-        self.backgroundColor = UIColor.red
+        self.backgroundColor = UIColor.white
+        
+        let viewBorder:UIView = UIView()
+        viewBorder.translatesAutoresizingMaskIntoConstraints = false
+        viewBorder.isUserInteractionEnabled = false
+        viewBorder.backgroundColor = UIColor.black
+        
+        self.addSubview(viewBorder)
+        
+        viewBorder.layoutTopToTop(view:self)
+        viewBorder.layoutHeight(constant:1)
+        viewBorder.layoutEqualsHorizontal(view:self)
     }
     
     required init?(coder:NSCoder) {
