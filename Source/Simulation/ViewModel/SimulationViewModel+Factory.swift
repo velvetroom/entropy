4a761c4b10bb9ddb8ab1535f7159ed1140999591
Zero <zero.griffin@gmail.com>
2018-03-12 16:54:09 +0100

    Add languages to factory

--- Source/Simulation/ViewModel/SimulationViewModel+Factory.swift
+++ Source/Simulation/ViewModel/SimulationViewModel+Factory.swift
@@ -4,7 +4,8 @@ extension SimulationViewModel {
     class func factoryMenu() -> SimulationViewModelMenu {
         let items:[SimulationViewModelMenuProtocol] = [
             SimulationViewModelMenuProject(),
-            SimulationViewModelMenuContributors()]
+            SimulationViewModelMenuContributors(),
+            SimulationViewModelMenuLanguages()]
         var menu:SimulationViewModelMenu = SimulationViewModelMenu()
         menu.items = items
         return menu
