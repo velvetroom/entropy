f113d2938780ed00bcb4479711036b2842dbdce7
Zero <zero.griffin@gmail.com>
2018-03-12 08:54:01 +0100

    Dequeue menu cells

--- Source/Simulation/Controller/SimulationControllerMenu.swift
+++ Source/Simulation/Controller/SimulationControllerMenu.swift
@@ -1,10 +1,19 @@
-import Foundation
+import UIKit
 
-class SimulationControllerMenu:NSObject {
+class SimulationControllerMenu:NSObject,
+    UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     var viewModel:[SimulationViewModelMenuProtocol]
     
     override init() {
         self.viewModel = []
         super.init()
+    }
+    
+    func collectionView(_:UICollectionView, numberOfItemsInSection:Int) -> Int {
+        return self.viewModel.count
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt index:IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier:SimulationViewMenu.Constants.cell, for:index)
     }
 }
