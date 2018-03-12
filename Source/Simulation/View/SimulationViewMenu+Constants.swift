8fcd37d4860526c3bd69789ff3f915ab29ced394
Zero <zero.griffin@gmail.com>
2018-03-12 16:56:33 +0100

    Reduce menu cell width

--- Source/Simulation/View/SimulationViewMenu+Constants.swift
+++ Source/Simulation/View/SimulationViewMenu+Constants.swift
@@ -3,6 +3,6 @@ import UIKit
 extension SimulationViewMenu {
     struct Constants {
         static let cell:String = "cell"
-        static let cellWidth:CGFloat = 180
+        static let cellWidth:CGFloat = 130
     }
 }
