d80a3918d06da0356d43704deab7609335e501bb
Zero <zero.griffin@gmail.com>
2018-03-13 12:44:53 +0100

    Test contributors content is not empty

---
+++ Tests/Simulation/TestSimulationViewModelMenuContributors.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import entropy
+
+class TestSimulationViewModelMenuContributors:XCTestCase {
+    private var item:SimulationViewModelMenuContributors?
+    
+    override func setUp() {
+        super.setUp()
+        self.item = SimulationViewModelMenuContributors()
+    }
+    
+    func testViewModelContentIsNotEmpty() {
+        XCTAssertNotNil(self.item, "Unable to load item")
+        guard
+            let viewModelContent:SimulationViewModelContent = self.item?.factoryViewModel()
+        else {
+            return
+        }
+        XCTAssertGreaterThan(0, viewModelContent.items.count, "Content is empty")
+    }
+}
