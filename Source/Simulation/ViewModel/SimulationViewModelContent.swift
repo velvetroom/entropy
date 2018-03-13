06272c91eb6397a6c15c3ed43d7088d7b6a64033
Zero <zero.griffin@gmail.com>
2018-03-13 12:36:09 +0100

    Set view model content comparable

--- Source/Simulation/ViewModel/SimulationViewModelContent.swift
+++ Source/Simulation/ViewModel/SimulationViewModelContent.swift
@@ -1,5 +1,31 @@
 import Foundation
 
-struct SimulationViewModelContent {
+struct SimulationViewModelContent:Equatable {
+    var items:[SimulationViewModelContentProtocol]
     
+    init() {
+        self.items = []
+    }
+    
+    static func ==(lhs:SimulationViewModelContent, rhs:SimulationViewModelContent) -> Bool {
+        let lhsCount:Int = lhs.items.count
+        let rhsCount:Int = rhs.items.count
+        guard
+            lhsCount == rhsCount
+        else {
+            return false
+        }
+        for index:Int in 0 ..< lhsCount {
+            let leftItem:SimulationViewModelContentProtocol = lhs.items[index]
+            let rightItem:SimulationViewModelContentProtocol = rhs.items[index]
+            let leftItemType:SimulationViewModelContentProtocol.Type = type(of:leftItem)
+            let rightItemType:SimulationViewModelContentProtocol.Type = type(of:rightItem)
+            guard
+                leftItemType == rightItemType
+            else {
+                return false
+            }
+        }
+        return true
+    }
 }
