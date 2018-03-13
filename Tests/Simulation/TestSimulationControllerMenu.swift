388c2a0a7615fab644df69bd51a6692673ff28b2
Zero <zero.griffin@gmail.com>
2018-03-13 10:56:59 +0100

    Add test for did select item at index

--- Tests/Simulation/TestSimulationControllerMenu.swift
+++ Tests/Simulation/TestSimulationControllerMenu.swift
@@ -1,27 +1,16 @@
 import XCTest
+@testable import entropy
 
-class TestSimulationControllerMenu: XCTestCase {
-    
+class TestSimulationControllerMenu:XCTestCase {
+    private var menu:SimulationControllerMenu?
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.menu = SimulationControllerMenu()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testDidSelectItem() {
+        let collectionView:UICollectionView = UICollectionView()
+        let index:IndexPath = IndexPath(item:1, section:0)
+        self.menu.selectedItem(index:index, in collectionView:collectionView)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
