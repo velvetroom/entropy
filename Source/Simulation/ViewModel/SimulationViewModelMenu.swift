0cc1b635a478ebdfc032e774ea24594b2a5094f8
Zero <zero.griffin@gmail.com>
2018-03-13 15:45:28 +0100

    Add test for selected item

--- Source/Simulation/ViewModel/SimulationViewModelMenu.swift
+++ Source/Simulation/ViewModel/SimulationViewModelMenu.swift
@@ -3,12 +3,12 @@ import Foundation
 class SimulationViewModelMenu {
     var items:[SimulationViewModelMenuProtocol]
     var selected:IndexPath
-    /*
+    
     var selectedItem:SimulationViewModelMenuProtocol {
         get {
             return self.items[selected.item]
         }
-    }*/
+    }
     
     init() {
         self.items = []
--- Tests/Simulation/TestSimulationViewModel.swift
+++ Tests/Simulation/TestSimulationViewModel.swift
@@ -8,4 +8,12 @@ class TestSimulationViewModel:XCTestCase {
         super.setUp()
         self.viewModel = SimulationViewModel()
     }
+    
+    func testViewModelInstantiated() {
+        XCTAssertNotNil(self.viewModel, "Failed creating view model")
+    }
+    
+    func testMenuReturnsSelectedItem() {
+        XCTAssertNotNil(self.viewModel?.menu.selectedItem, "Failed to return selected item")
+    }
 }
