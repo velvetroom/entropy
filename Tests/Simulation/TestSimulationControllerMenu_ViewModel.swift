5db84d71fa3ae75dae7b041b7dc43a6ed1d64cd0
Zero <zero.griffin@gmail.com>
2018-03-13 11:59:42 +0100

    Test factory view model

--- Tests/Simulation/TestSimulationControllerMenu_ViewModel.swift
+++ Tests/Simulation/TestSimulationControllerMenu_ViewModel.swift
@@ -2,10 +2,43 @@ import XCTest
 @testable import entropy
 
 class TestSimulationControllerMenu_ViewModel:XCTestCase {
+    private var controller:SimulationController?
     private var menu:SimulationControllerMenu?
+    private var viewModel:SimulationViewModel?
+    private struct Constants {
+        static let selectingIndex:Int = 1
+    }
     
     override func setUp() {
         super.setUp()
         self.menu = SimulationControllerMenu()
+        self.controller = SimulationController()
+        self.viewModel = SimulationViewModel()
+        self.controller?.viewModel = self.viewModel
+        
+        if let viewModel:SimulationViewModel = self.viewModel {
+            self.menu?.reloadMenu(viewModel:viewModel.menu)
+        }
+    }
+    
+    func testViewModelUpdated() {
+        self.selectMenuAtIndex(index:Constants.selectingIndex)
+        
+    }
+    
+    private func selectMenuAtIndex(index:Int) {
+        let indexPath:IndexPath = IndexPath(item:index, section:0)
+        self.menu?.selectedItem(index:indexPath)
+    }
+    
+    private func validateIndexSelectedIsViewModel(index:Int) {
+        let menuItem:SimulationViewModelMenuProtocol? = self.viewModel?.menu.items[index]
+        XCTAssertNotNil(menuItem, "Unable to load menu item")
+        guard
+            let item:SimulationViewModelMenuProtocol = menuItem
+        else {
+            return
+        }
+        let itemViewModel:SimulationViewModelContent = item.factoryViewModel()
     }
 }
