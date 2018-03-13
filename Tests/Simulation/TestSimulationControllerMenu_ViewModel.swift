7feaf3c6e26d131fda140f21a4b87ca6cc2768e7
Zero <zero.griffin@gmail.com>
2018-03-13 11:42:44 +0100

    Create test for simulation menu view model

---
+++ Tests/Simulation/TestSimulationControllerMenu_ViewModel.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import entropy
+
+class TestSimulationControllerMenu_ViewModel:XCTestCase {
+    private var menu:SimulationControllerMenu?
+    
+    override func setUp() {
+        super.setUp()
+        self.menu = SimulationControllerMenu()
+    }
+}
