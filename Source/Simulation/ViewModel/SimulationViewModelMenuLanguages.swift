2f1395cae6aa1711d71cd3239ef9ddb554e8243d
Zero <zero.griffin@gmail.com>
2018-03-13 17:10:04 +0100

    Add items to content view model

--- Source/Simulation/ViewModel/SimulationViewModelMenuLanguages.swift
+++ Source/Simulation/ViewModel/SimulationViewModelMenuLanguages.swift
@@ -8,7 +8,10 @@ class SimulationViewModelMenuLanguages:SimulationViewModelMenuProtocol {
     }
     
     func factoryViewModel() -> SimulationViewModelContent {
+        let items:[SimulationViewModelContentProtocol] = [
+            SimulationViewModelContentAddLanguage()]
         let content:SimulationViewModelContent = SimulationViewModelContent()
+        content.items = items
         return content
     }
 }
