dded0f4bca2b93dcaf9161bcd439b2274c4bdf82
Zero <zero.griffin@gmail.com>
2018-03-14 14:29:54 +0100

    Create project name cell constants extension

---
+++ Source/Simulation/View/SimulationViewContentCellProjectName+Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension SimulationViewContentCellProjectName {
+    struct Constants {
+        static let titleHeight:CGFloat = 20
+        static let titleFontSize:CGFloat = 16
+        static let fieldHeight:CGFloat = 32
+        static let fieldHorizontalMargin:CGFloat = 20
+    }
+}
--- Source/Simulation/View/SimulationViewContentCellProjectName.swift
+++ Source/Simulation/View/SimulationViewContentCellProjectName.swift
@@ -1,8 +1,16 @@
 import UIKit
 
 class SimulationViewContentCellProjectName:SimulationViewContentCell {
+    private(set) weak var viewField:UITextField?
+    
     override init(frame:CGRect) {
         super.init(frame:frame)
+        
+        let viewField:UITextField = UITextField()
+        self.viewField = viewField
+        
+        self.addSubview(viewField)
+        
     }
     
     required init?(coder:NSCoder) {
