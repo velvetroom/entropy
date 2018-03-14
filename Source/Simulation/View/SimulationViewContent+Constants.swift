c2d2120a7e297342032aa72c2e8199d4cb3bbb91
Zero <zero.griffin@gmail.com>
2018-03-14 12:30:09 +0100

    Create constants extension

---
+++ Source/Simulation/View/SimulationViewContent+Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension SimulationViewContent {
+    struct Constants {
+        static let cellProjectName:String = "projectName"
+        static let cellAddContributor:String = "addContributor"
+        static let cellAddLanguage:String = "addLanguage"
+    }
+}
