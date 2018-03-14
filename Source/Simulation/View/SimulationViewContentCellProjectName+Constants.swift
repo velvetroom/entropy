99dc7a66a1a427f749e32dd3c876352ac49bf8a8
Zero <zero.griffin@gmail.com>
2018-03-14 14:43:07 +0100

    Layout view title

--- Source/Simulation/View/SimulationViewContentCellProjectName+Constants.swift
+++ Source/Simulation/View/SimulationViewContentCellProjectName+Constants.swift
@@ -4,6 +4,7 @@ extension SimulationViewContentCellProjectName {
     struct Constants {
         static let titleHeight:CGFloat = 20
         static let titleFontSize:CGFloat = 16
+        static let titleLeft:CGFloat = 20
         static let fieldHeight:CGFloat = 32
         static let fieldHorizontalMargin:CGFloat = 20
     }
--- Source/Simulation/View/SimulationViewContentCellProjectName.swift
+++ Source/Simulation/View/SimulationViewContentCellProjectName.swift
@@ -1,16 +1,30 @@
 import UIKit
 
 class SimulationViewContentCellProjectName:SimulationViewContentCell {
+    private(set) weak var viewTitle:UILabel?
     private(set) weak var viewField:UITextField?
     
     override init(frame:CGRect) {
         super.init(frame:frame)
         
+        let viewTitle:UILabel = UILabel()
+        viewTitle.translatesAutoresizingMaskIntoConstraints = false
+        viewTitle.backgroundColor = UIColor.clear
+        viewTitle.isUserInteractionEnabled = false
+        viewTitle.font = UIFont.systemFont(ofSize:Constants.titleFontSize, weight:UIFont.Weight.light)
+        viewTitle.textColor = UIColor.black
+        self.viewTitle = viewTitle
+        
         let viewField:UITextField = UITextField()
         self.viewField = viewField
         
+        self.addSubview(viewTitle)
         self.addSubview(viewField)
         
+        viewTitle.layoutTopToTop(view:self)
+        viewTitle.layoutHeight(constant:Constants.titleHeight)
+        viewTitle.layoutLeftToLeft(view:self, constant:Constants.titleLeft)
+        viewTitle.layoutWidthGreaterOrEqual()
     }
     
     required init?(coder:NSCoder) {
