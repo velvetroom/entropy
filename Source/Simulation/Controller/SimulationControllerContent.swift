fc1cd7135985e822aa2cdbf73cf92eac0d081955
Zero <zero.griffin@gmail.com>
2018-03-14 09:20:59 +0100

    Make controller conform to collection view delegate/dataSource

--- Source/Simulation/Controller/SimulationControllerContent.swift
+++ Source/Simulation/Controller/SimulationControllerContent.swift
@@ -1,5 +1,11 @@
-import Foundation
+import UIKit
 
-class SimulationControllerContent:NSObject {
+class SimulationControllerContent:NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_:UICollectionView, numberOfItemsInSection:Int) -> Int {
+        return  0
+    }
     
+    func collectionView(_:UICollectionView, cellForItemAt index:IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 }
--- Tests/Simulation/TestSimulationControllerContent.swift
+++ Tests/Simulation/TestSimulationControllerContent.swift
@@ -12,4 +12,14 @@ class TestSimulationControllerContent:XCTestCase {
     func testInitialisation() {
         XCTAssertNotNil(self.controller, "Unable to create controller")
     }
+    
+    func testControllerIsCollectionViewDelegate() {
+        let delegate:UICollectionViewDelegate? = self.controller as? UICollectionViewDelegate
+        XCTAssertNotNil(delegate, "Controller doesn't conform to collection view delegate")
+    }
+    
+    func testControllerIsCollectionViewDatasource() {
+        let dataSource:UICollectionViewDataSource? = self.controller as? UICollectionViewDataSource
+        XCTAssertNotNil(dataSource, "Controller doesn't conform to collection view dataSource")
+    }
 }
