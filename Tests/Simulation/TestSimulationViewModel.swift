30a588574a7f7f3851d7356075c4ddbddcfcfc8d
Zero <zero.griffin@gmail.com>
2018-03-14 15:45:09 +0100

    Test validate project gets updated in menu

--- Tests/Simulation/TestSimulationViewModel.swift
+++ Tests/Simulation/TestSimulationViewModel.swift
@@ -2,15 +2,25 @@ import XCTest
 @testable import entropy
 
 class TestSimulationViewModel:XCTestCase {
+    private var project:Project?
     private var viewModel:SimulationViewModel?
+    private struct Constants {
+        static let identifier:String = "project"
+        static let entropy:Float = 1
+    }
     
     override func setUp() {
         super.setUp()
+        self.project = Project(identifier:Constants.identifier, entropy:Constants.entropy)
         self.viewModel = SimulationViewModel()
     }
     
     func testViewModelInstantiated() {
         XCTAssertNotNil(self.viewModel, "Failed creating view model")
+    }
+    
+    func testProjectInstantiated() {
+        XCTAssertNotNil(self.project, "Project doesn't get instantiated")
     }
     
     func testMenuReturnsSelectedItem() {
@@ -24,5 +34,30 @@ class TestSimulationViewModel:XCTestCase {
             return
         }
         XCTAssertGreaterThan(countItems, 0, "Error: Empty menu items")
+    }
+    
+    func testUpdateProject() {
+        self.updateProject()
+        self.validateMenuItemsHaveProject()
+    }
+    
+    private func updateProject() {
+        guard
+            let project:Project = self.project
+        else {
+            return
+        }
+        self.viewModel?.update(project:project)
+    }
+    
+    private func validateMenuItemsHaveProject() {
+        guard
+            let items:[SimulationViewModelMenuProtocol] = self.viewModel?.menu.items
+        else {
+            return
+        }
+        for item:SimulationViewModelMenuProtocol in items {
+            XCTAssertNotNil(item.project, "Item doesn't have a reference to project")
+        }
     }
 }
