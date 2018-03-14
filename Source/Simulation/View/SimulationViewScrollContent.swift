3def21a1a640e5c136b43d775d65baab64e0bea8
Zero <zero.griffin@gmail.com>
2018-03-14 08:09:43 +0100

    Create view scroll content

---
+++ Source/Simulation/View/SimulationViewScrollContent.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class SimulationViewScrollContent:UICollectionViewCell {
+    override init(frame:CGRect) {
+        super.init(frame:frame)
+        self.clipsToBounds = true
+        self.backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder:NSCoder) {
+        return nil
+    }
+}
