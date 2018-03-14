f2bb0a9e56fea7eb6be23398e21989adb02b07dc
Zero <zero.griffin@gmail.com>
2018-03-14 16:44:46 +0100

    Test project name gets updated

--- Tests/Simulation/TestSimulationViewContentCellProjectName.swift
+++ Tests/Simulation/TestSimulationViewContentCellProjectName.swift
@@ -3,13 +3,50 @@ import XCTest
 
 class TestSimulationViewContentCellProjectName:XCTestCase {
     private var cell:SimulationViewContentCellProjectName?
+    private var viewModel:SimulationViewModelContentProjectName?
+    private var project:Project?
+    private struct Constants {
+        static let identifier:String = "project"
+        static let entropy:Float = 1
+        static let projectName:String = "test"
+    }
     
     override func setUp() {
         super.setUp()
         self.cell = SimulationViewContentCellProjectName()
+        self.viewModel = SimulationViewModelContentProjectName()
+        self.project = Project(identifier:Constants.identifier, entropy:Constants.entropy)
+        self.viewModel?.project = self.project
     }
     
     func testInitialisation() {
         XCTAssertNotNil(self.cell, "Failed to create project name cell")
+        XCTAssertNotNil(self.viewModel, "Failed to create view model")
+        XCTAssertNotNil(self.project, "Failed to create project")
+    }
+    
+    func testUpdateProject() {
+        self.updateViewModel()
+        self.validateProjectNameUpdated()
+    }
+    
+    private func updateViewModel() {
+        guard
+            let viewModel:SimulationViewModelContentProjectName = self.viewModel
+        else {
+            return
+        }
+        self.cell?.update(viewModel:viewModel)
+    }
+    
+    private func validateProjectNameUpdated() {
+        let projectName:String? = self.cell?.viewField?.text
+        XCTAssertNotNil(projectName, "No project name found")
+        guard
+            let name:String = projectName
+        else {
+            return
+        }
+        XCTAssertEqual(name, Constants.projectName, "Project name is not being updated")
     }
 }
