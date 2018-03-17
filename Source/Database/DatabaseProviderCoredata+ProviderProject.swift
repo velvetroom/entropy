c60a1b6782cea66ce46100666ea1c8a4e7ed0be2
Zero <zero.griffin@gmail.com>
2018-03-17 09:38:12 +0100

    Add load project from coredata

--- Source/Database/DatabaseProviderCoredata+ProviderProject.swift
+++ Source/Database/DatabaseProviderCoredata+ProviderProject.swift
@@ -5,7 +5,7 @@ extension DatabaseProviderCoredata {
     private var projectRequest:NSFetchRequest<CoredataProject> {
         get {
             let request:NSFetchRequest<CoredataProject> = NSFetchRequest(entityName:CoredataProject.name)
-            request.fetchLimit = 0
+            request.fetchLimit = 1
             request.returnsObjectsAsFaults = false
             return request
         }
@@ -27,7 +27,26 @@ extension DatabaseProviderCoredata {
     }
     
     func loadProject(identifier:String, found:@escaping((Project) -> ()), notFound:@escaping(() -> ())) {
-        
+        let request:NSFetchRequest<CoredataProject> = self.factoryRequestToLoadProject(identifier:identifier)
+        self.load(request:request) { (coredataProjects:[CoredataProject]) in
+            guard
+                let coredataProject:CoredataProject = coredataProjects.first,
+                let project:Project = coredataProject.factoryProject()
+            else {
+                notFound()
+            }
+            found(project)
+        }
+    }
+    
+    private func factoryRequestToLoadProject(identifier:String) -> NSFetchRequest<CoredataProject> {
+        let request:NSFetchRequest<CoredataProject> = self.projectRequest
+        request.predicate = self.factoryPredicateToLoadProject(identifier:identifier)
+        return request
+    }
+    
+    private func factoryPredicateToLoadProject(identifier:String) -> NSPredicate {
+        return NSPredicate(format:"identifier = \"%@\"", identifier)
     }
     
     func save(project:Project, completion:@escaping(() -> ())) {
